aa95fff442b08119ebb7ec805dd4586ae054b5c6
Dillon Kearns <dillon@incrementalelm.com>
2023-07-19 10:07:01 -0700

    Handle creating from usage for qualified examples.

--- src/main/kotlin/org/elm/ide/inspections/MakeDeclarationFromUsageFix.kt
+++ src/main/kotlin/org/elm/ide/inspections/MakeDeclarationFromUsageFix.kt
@@ -3,8 +3,8 @@ package org.elm.ide.inspections
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import org.elm.lang.core.psi.ElmPsiFactory
-import org.elm.lang.core.psi.elements.ElmFunctionCallExpr
-import org.elm.lang.core.psi.elements.ElmValueExpr
+import org.elm.lang.core.psi.elements.*
+import org.elm.lang.core.stubs.index.ElmModulesIndex
 
 class MakeDeclarationFromUsageFix : NamedQuickFix("Create") {
     override fun applyFix(element: PsiElement, project: Project) {
@@ -12,7 +12,6 @@ class MakeDeclarationFromUsageFix : NamedQuickFix("Create") {
            return
         }
 
-        // addTopLevelFunction("greet", psiFactory)
         val psiFactory = ElmPsiFactory(project)
         val s = element.referenceName
 
@@ -21,9 +20,28 @@ class MakeDeclarationFromUsageFix : NamedQuickFix("Create") {
 
         val argList: String = argListFromArity(arity)
         val valueDeclaration =
-           psiFactory.createTopLevelFunction("$s $argList= Debug.todo \"TODO\"")
-        // spliceIntoTopLevel
-        element.elmFile.add(valueDeclaration)
+           psiFactory.createTopLevelFunction("$s $argList= Debug.todo \"NOT IMPLEMENTED\"")
+
+        when (element.flavor) {
+            Flavor.QualifiedValue -> {
+                val elmModule = ElmModulesIndex.get(element.qid.qualifierPrefix, element.elmFile)
+                val targetFile = elmModule?.elmFile
+                if (targetFile != null) {
+                    targetFile.add(valueDeclaration)
+                    val elmExposingList = elmModule.exposingList
+                    elmExposingList?.addItem(element.referenceName)
+                }
+            }
+            Flavor.BareValue -> {
+                element.elmFile.add(valueDeclaration)
+            }
+            Flavor.QualifiedConstructor -> {
+                // TODO - handle Constructors
+            }
+            Flavor.BareConstructor -> {
+                // TODO - handle Constructors
+            }
+        }
     }
 
     private fun argListFromArity(arity: Int): String {
