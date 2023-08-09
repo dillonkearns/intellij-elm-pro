d3e49065d27e0d83a4ffb08bbb0be1294aa1e686
Dillon Kearns <dillon@incrementalelm.com>
2023-08-09 09:20:04 -0700

    Remove unused and fix doc comment text.

--- src/main/kotlin/org/elm/lang/core/psi/elements/DocsAnnotationItem.kt
+++ src/main/kotlin/org/elm/lang/core/psi/elements/DocsAnnotationItem.kt
@@ -1,27 +1,22 @@
 package org.elm.lang.core.psi.elements
 
 import com.intellij.lang.ASTNode
-import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.IStubElementType
-import com.intellij.psi.tree.IElementType
-import org.elm.lang.core.psi.ElmExposedItemTag
 import org.elm.lang.core.psi.ElmStubbedElement
-import org.elm.lang.core.psi.ElmTypes.*
-import org.elm.lang.core.psi.parentOfType
+import org.elm.lang.core.psi.ElmTypes.LOWER_CASE_IDENTIFIER
+import org.elm.lang.core.psi.ElmTypes.UPPER_CASE_IDENTIFIER
 import org.elm.lang.core.psi.tokenSetOf
 import org.elm.lang.core.resolve.ElmReferenceElement
 import org.elm.lang.core.resolve.reference.DocsValueModuleReference
 import org.elm.lang.core.resolve.reference.ElmReference
-import org.elm.lang.core.resolve.reference.ExposedValueImportReference
-import org.elm.lang.core.resolve.reference.ExposedValueModuleReference
 import org.elm.lang.core.stubs.ElmPlaceholderRefStub
 
 
 /**
- * A value or function exposed via an import clause's `exposing` list.
+ * A value or type within an @docs annotation list.
  *
- * e.g. `bar` in `import Foo exposing (bar)`
+ * e.g. `Decoder` and `succeed` in `@docs Decoder, succeed`.
  */
 class DocsAnnotationItem : ElmStubbedElement<ElmPlaceholderRefStub>, ElmReferenceElement {
 
@@ -32,24 +27,19 @@ class DocsAnnotationItem : ElmStubbedElement<ElmPlaceholderRefStub>, ElmReferenc
             super(stub, stubType)
 
 
-    val lowerCaseIdentifier: PsiElement
-        get() = findNotNullChildByType(LOWER_CASE_IDENTIFIER)
-
-
     override val referenceNameElement: PsiElement
         get() =
-//            findChildByType(LOWER_CASE_IDENTIFIER)
-
-            findChildByClass(ElmExposedOperator::class.java) ?:
-            findNotNullChildByType(tokenSetOf(LOWER_CASE_IDENTIFIER, UPPER_CASE_IDENTIFIER))
-//            lowerCaseIdentifier
+            findChildByClass(ElmExposedOperator::class.java) ?: findNotNullChildByType(
+                tokenSetOf(
+                    LOWER_CASE_IDENTIFIER,
+                    UPPER_CASE_IDENTIFIER
+                )
+            )
 
     override val referenceName: String
         get() = stub?.refName ?: referenceNameElement.text
 
-    override fun getReference(): ElmReference
-        {
-            return DocsValueModuleReference(this)
-        }
-
+    override fun getReference(): ElmReference {
+        return DocsValueModuleReference(this)
+    }
 }
