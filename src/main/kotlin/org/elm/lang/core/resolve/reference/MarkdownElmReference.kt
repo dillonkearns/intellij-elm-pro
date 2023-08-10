37af5e4e8d3c6168c575d0a35c586af53222363e
Dillon Kearns <dillon@incrementalelm.com>
2023-08-10 10:30:40 -0700

    Fix reference resolution for Markdown Elm References.

--- src/main/kotlin/org/elm/lang/core/resolve/reference/MarkdownElmReference.kt
+++ src/main/kotlin/org/elm/lang/core/resolve/reference/MarkdownElmReference.kt
@@ -1,23 +1,33 @@
 package org.elm.lang.core.resolve.reference
 
+import com.intellij.util.containers.headTail
+import com.intellij.util.containers.headTailOrNull
 import org.elm.lang.core.psi.ElmNamedElement
 import org.elm.lang.core.psi.elements.MarkdownElmRef
 import org.elm.lang.core.resolve.scope.DeclaredNames
 import org.elm.lang.core.resolve.scope.GlobalScope
 import org.elm.lang.core.resolve.scope.ModuleScope
+import org.elm.lang.core.stubs.index.ElmModulesIndex
 
-class MarkdownElmReference(docsItem: MarkdownElmRef)
-    : ElmReferenceCached<MarkdownElmRef>(docsItem) {
+class MarkdownElmReference(docsItem: MarkdownElmRef) : ElmReferenceCached<MarkdownElmRef>(docsItem) {
 
-    override fun resolveInner(): ElmNamedElement? = getCandidates()[element.referenceName]
-
-    override fun getVariants(): Array<ElmNamedElement> = getCandidates().array
-
-    private fun getCandidates(): DeclaredNames {
-        // TODO does this account for non-imported modules?
-        val globalScope = GlobalScope.forElmFile(element.elmFile)
-        val declaredValues = ModuleScope.getDeclaredValues(element.elmFile)
-        val declaredTypes = ModuleScope.getDeclaredTypes(element.elmFile)
-        return declaredValues.plus(declaredTypes).plus(DeclaredNames(globalScope?.getVisibleTypes() ?: emptyList()))
+    override fun resolveInner(): ElmNamedElement? {
+        val splitList = element.text.split('#')
+        val targetModule = splitList[0].ifEmpty { null }
+        val targetValueTypeOrOperator = splitList.getOrNull(1)?.ifEmpty { null }
+        val thisFile = element.elmFile
+        if (targetModule == null) {
+            return ModuleScope.getDeclaredValues(thisFile)
+                .plus(ModuleScope.getDeclaredTypes(thisFile))[targetValueTypeOrOperator]
+        } else {
+            val resolvedTargetModule = ElmModulesIndex.get(targetModule, thisFile)?.elmFile ?: return null
+            return if (targetValueTypeOrOperator == null) {
+                resolvedTargetModule.getModuleDecl()
+            } else {
+                ModuleScope.getDeclaredValues(resolvedTargetModule).plus(ModuleScope.getDeclaredTypes(resolvedTargetModule))[targetValueTypeOrOperator]
+            }
+        }
     }
+
+    override fun getVariants(): Array<ElmNamedElement> = emptyArray()
 }
