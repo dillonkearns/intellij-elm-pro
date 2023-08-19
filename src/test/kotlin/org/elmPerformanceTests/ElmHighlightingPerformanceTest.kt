388f33daf122005330a0868819a4618a90f6c920
Dillon Kearns <dillon@incrementalelm.com>
2023-08-19 12:43:10 -0700

    Upgrade changes.

--- src/test/kotlin/org/elmPerformanceTests/ElmHighlightingPerformanceTest.kt
+++ src/test/kotlin/org/elmPerformanceTests/ElmHighlightingPerformanceTest.kt
@@ -33,8 +33,8 @@ import org.elm.utils.Timings
 
 class ElmHighlightingPerformanceTest : ElmRealProjectTestBase() {
 
-    // It is a performance test, but we will measure performance ourselves
-    override fun isPerformanceTest(): Boolean = false
+//    // It is a performance test, but we will measure performance ourselves
+//    override fun isPerformanceTest(): Boolean = false
 
     fun `test highlighting elm-json-tree-view`() =
             repeatTest { highlightProjectFile(JSON_TREE_VIEW, "src/JsonTree.elm") }
@@ -93,5 +93,5 @@ class ElmHighlightingPerformanceTest : ElmRealProjectTestBase() {
     }
 
     private fun currentPsiModificationCount() =
-            PsiModificationTracker.SERVICE.getInstance(project).modificationCount
+            PsiModificationTracker.getInstance(project).modificationCount
 }
