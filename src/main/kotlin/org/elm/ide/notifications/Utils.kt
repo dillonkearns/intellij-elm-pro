61370da15aa9b35fbfc4462937215e28a3fef8ed
Dillon Kearns <dillon@incrementalelm.com>
2023-11-21 11:03:37 -0800

    Add additional helper to show error bubble with title.

--- src/main/kotlin/org/elm/ide/notifications/Utils.kt
+++ src/main/kotlin/org/elm/ide/notifications/Utils.kt
@@ -7,10 +7,7 @@
 
 package org.elm.ide.notifications
 
-import com.intellij.notification.NotificationAction
-import com.intellij.notification.NotificationGroup
-import com.intellij.notification.NotificationType
-import com.intellij.notification.Notifications
+import com.intellij.notification.*
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataContext
@@ -31,9 +28,11 @@ private val pluginNotifications = NotificationGroup.balloonGroup("Elm Plugin")
 fun Project.showBalloon(
         content: String,
         type: NotificationType,
+        title: String,
         vararg actions: Pair<String, (() -> Unit)>
 ) {
     val notification = pluginNotifications.createNotification(content, type)
+    notification.setTitle(title)
     actions.forEach { (title, fn) ->
         notification.addAction(
                 NotificationAction.create(title) { _, notif ->
@@ -44,6 +43,32 @@ fun Project.showBalloon(
     }
     Notifications.Bus.notify(notification, this)
 }
+
+/**
+ * Show a balloon notification along with action(s). The notification will be automatically dismissed
+ * when an action is invoked.
+ *
+ * @param content The main content to be shown in the notification
+ * @param type The notification type
+ * @param actions Optional list of actions to be included in the notification
+ */
+fun Project.showBalloon(
+    content: String,
+    type: NotificationType,
+    vararg actions: Pair<String, (() -> Unit)>
+) {
+    val notification = pluginNotifications.createNotification(content, type)
+    actions.forEach { (title, fn) ->
+        notification.addAction(
+            NotificationAction.create(title) { _, notif ->
+                notif.hideBalloon()
+                fn()
+            }
+        )
+    }
+    Notifications.Bus.notify(notification, this)
+}
+
 
 fun executeAction(action: AnAction, place: String, dataContext: DataContext) {
     val event = AnActionEvent.createFromAnAction(action, null, place, dataContext)
