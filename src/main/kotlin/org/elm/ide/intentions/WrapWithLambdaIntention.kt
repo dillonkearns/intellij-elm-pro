481283341826ac8dda88c79bbec7b3b9a033f30e
Dillon Kearns <dillon@incrementalelm.com>
2024-07-19 16:10:15 -0700

    Fix wrap with lambda intention type resolution.

--- src/main/kotlin/org/elm/ide/intentions/WrapWithLambdaIntention.kt
+++ src/main/kotlin/org/elm/ide/intentions/WrapWithLambdaIntention.kt
@@ -7,22 +7,36 @@ import org.elm.ide.refactoring.uniqueValueName
 import org.elm.lang.core.psi.ElmExpressionTag
 import org.elm.lang.core.psi.ElmPsiFactory
 import org.elm.lang.core.psi.ancestorOrSelf
+import org.elm.lang.core.psi.elementType
+import org.elm.lang.core.psi.elements.ElmValueExpr
 import org.elm.lang.core.psi.elements.ElmValueQID
 import org.elm.lang.core.types.TyFunction
 import org.elm.lang.core.types.findInference
+import org.elm.lang.core.types.findTy
 
 class WrapWithLambdaIntention : ElmAtCaretIntentionActionBase<WrapWithLambdaIntention.Context>() {
     data class Context(
         val expression: ElmValueQID,
     )
 
-    override fun getText() = "Wrap a function with a lambda"
+    override fun getText() = "Wrap with (\\a -> ... a)"
     override fun getFamilyName() = text
 
-    override fun findApplicableContext(project: Project, editor: Editor, element: PsiElement): Context? =
-        (element.findInference()?.ty as? TyFunction)?.let {
-            element.ancestorOrSelf<ElmValueQID>()?.let { qid -> Context(qid) }
+    override fun findApplicableContext(project: Project, editor: Editor, element: PsiElement): Context? {
+        val expression = element.parent?.parent as? ElmValueExpr
+        if (element.elementType.toString() == "LOWER_CASE_IDENTIFIER" && element.parent is ElmValueQID && expression != null) {
+            element.findInference()
+            return (expression.findTy() as? TyFunction)?.let {
+                if (it.parameters.isNotEmpty()) {
+                    element.ancestorOrSelf<ElmValueQID>()?.let { qid -> Context(qid) }
+                } else {
+                    null
+                }
+            }
+        } else {
+            return null
         }
+    }
 
     override fun invoke(project: Project, editor: Editor, context: Context) {
         val factory = ElmPsiFactory(project)
